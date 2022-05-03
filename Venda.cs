52918df6942728d56c8f7529d8518db3dc99c265
Jonatan M Gomes <jonatan.mim@hotmail.com>
2022-05-02 22:26:41 -0300

    Criando métodos construtore e Getters e setters da classe: Venda.

--- Venda.cs
+++ Venda.cs
@@ -7,5 +7,52 @@ namespace ProjetoConcessionaria
         private Veiculo Veiculo { get; set; }
         private string FormaPagamento { get; set; }
         private double ValorFinal { get; set; }
+        public Venda(Cliente comprador, Funcionario vendedor, Veiculo veiculo, string formaPagamento)
+        {
+            SetComprador(comprador);
+            SetVendedor(vendedor);
+            SetVeiculo(veiculo);
+            SetFormaPagamento(formaPagamento);
+        }
+        public Cliente GetComprador()
+        {
+            return Comprador;
+        }
+        public void SetComprador(Cliente comprador)
+        {
+            Comprador = comprador;
+        }
+        public Funcionario GetVendedor()
+        {
+            return Vendedor;
+        }
+        public void SetVendedor(Funcionario vendedor)
+        {
+            Vendedor = vendedor;
+        }
+        public Veiculo GetVeiculo()
+        {
+            return Veiculo;
+        }
+        public void SetVeiculo(Veiculo veiculo)
+        {
+            Veiculo = veiculo;
+        }
+        public string GetFormaPagamento()
+        {
+            return FormaPagamento;
+        }
+        public void SetFormaPagamento(string formaPagamento)
+        {
+            FormaPagamento = formaPagamento;
+        }
+        public double GetValorFinal()
+        {
+            return ValorFinal;
+        }
+        public void SetValorFinal(double valorFinal)
+        {
+            ValorFinal = valorFinal;
+        }
     }
 }
