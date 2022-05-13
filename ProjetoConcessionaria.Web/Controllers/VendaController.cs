1521e7269df7deb1fdae0fd1baeb3d8fe6b583a5
Jonatan M Gomes <jonatan.mim@hotmail.com>
2022-05-13 17:54:10 -0300

    Criando controller para Venda

---
+++ ProjetoConcessionaria.Web/Controllers/VendaController.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjetoConcessionaria.Web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class VendaController : ControllerBase
+    {
+        public static List<Venda> VendasDaClasse { get; set; } = new List<Venda>();
+
+        [HttpGet("Get VendasDaLista")]
+        public IActionResult GetVendasDaLista()
+        {
+            return Ok(VendasDaClasse);
+        }
+
+        [HttpPost("Set VendaNaLista")]
+        public IActionResult SetVendaNaLista(Venda venda)
+        {
+            VendasDaClasse.Add(venda);
+            return Ok(VendasDaClasse);
+        }
+
+        [HttpDelete("Delete VendaDaLista")]
+        public IActionResult DeleteVendaDaLista()
+        {
+            VendasDaClasse.RemoveAt(VendasDaClasse.Count - 1);
+            return Ok(VendasDaClasse);
+        }
+    }
+}
